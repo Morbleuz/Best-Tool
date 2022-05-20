0e84adcb92e6920276abd7d7e791568dda56f0bb
Axel <axel2003@outlook.com>
2022-05-20 08:06:16 +0200

    ajout de fichier

--- bin/fail2ban.dart
+++ bin/fail2ban.dart
@@ -2,18 +2,12 @@ import 'dart:io';
 
 class Fail2Ban {
   static install() async {
-    await Process.run('bash', ['-c', "apt install fail2ban -y"]);
-    await Process.run('bash', ['-c', "apt install iptables -y"]);
-    await Process.run('bash', [
-      '-c',
-      "echo [pure-ftpd] >> /etc/fail2ban/jail.d/defaults-debian.conf"
-    ]);
-    await Process.run('bash', [
-      '-c',
-      "echo enable=true >> /etc/fail2ban/jail.d/defaults-debian.conf"
-    ]);
-    await Process.run('bash',
-        ['-c', "echo maxretry=3 >> /etc/fail2ban/jail.d/defaults-debian.conf"]);
-    await Process.run('echo bantime=30', ['-c', "apt install fail2ban -y"]);
+    await Process.run('bash', ['-c', 'apt install fail2ban -y']);
+    await Process.run('bash', ['-c', 'apt install iptables -y']);
+    await Process.run('bash', ['-c','echo [pure-ftpd] >> /etc/fail2ban/jail.d/defaults-debian.conf']);
+    await Process.run('bash', ['-c','echo enable=true >> /etc/fail2ban/jail.d/defaults-debian.conf']);
+    await Process.run('bash',['-c', 'echo maxretry=3 >> /etc/fail2ban/jail.d/defaults-debian.conf']);
+    await Process.run('bash',['-c', 'echo bantime=30 >> /etc/fail2ban/jail.d/defaults-debian.conf']);
+    await Process.run('bash', ['-c', 'apt install fail2ban -y']);
   }
 }
